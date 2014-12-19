9b5c1d384e47deed95db4aba55efea3d464ce31b
Mason Liu <mentor811@gmail.com>
2014-12-19 16:01:42 +0800

    Update LinearLayoutForListView.java

--- src/com/mentor/firstdemo/view/LinearLayoutForListView.java
+++ src/com/mentor/firstdemo/view/LinearLayoutForListView.java
@@ -9,37 +9,33 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.LinearLayout;
 
-public class LinearLayoutForListView extends LinearLayout {
+public class LinearLayoutLikeListView extends LinearLayout {
 
     private Adapter adapter;
-    private OnClickListener onClickListener = null;
+    private OnItemClickListener onItemClickListener = null;
     private int currentNum=-1;
     private boolean isFoot=false;
-    
-    public LinearLayoutForListView(Context context) {
+
+    public LinearLayoutLikeListView(Context context) {
         super(context);
-        
-
+        init();
     }
 
-    public LinearLayoutForListView(Context context, AttributeSet attrs) {
+    public LinearLayoutLikeListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
 
+    public void init(){
+        setOrientation(LinearLayout.VERTICAL);
     }
-    /**
-     * 设置点击监听
-     * 
-     * @param OnClickListener
-     */
-    public void setOnClickListener(OnClickListener onClickListener) {
-        this.onClickListener = onClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
-    /**
-     * 设置数据适配器
-     * 
-     * @param adpater
-     */
+
     public void setAdapter(Adapter adpater) {
+        if (adpater==null)return;
         this.adapter = adpater;
         bindLinearLayout();
     }
@@ -49,64 +45,66 @@ public class LinearLayoutForListView extends LinearLayout {
     public void bindLinearLayout() {
         int count = adapter.getCount();
         for (int i = currentNum+1; i < count; i++) {
+            final int a=i;
             View v = adapter.getView(i, null, null);
-            v.setOnClickListener(this.onClickListener);
+            v.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onClick(v, a);
+                }
+            });
             v.setVisibility(View.VISIBLE);
             currentNum=i;
             addView(v, i);
-            if(v==null)LogUtil.i("aaaa__null");
-            LogUtil.i("aaaa__");
-            
         }
     }
-    /**
-     * add more data
-     * 
-     * @param adpater
-     */
-    public void addMoreData() {
-       try{
-    	   bindLinearLayout();
-       }catch(Exception e){}
-    	
+
+    public void updateAdapter() {
+        try{
+            bindLinearLayout();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
- 
 
+    public void updateAllAdapter() {
+        try {
+            if (isFoot){
+                removeViews(0,getChildCount()-1);
+            }else{
+                removeAllViews();
+            }
+            currentNum=-1;
+            bindLinearLayout();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
-    /**
-     * ��ȡAdapter
-     * 
-     * @return adapter
-     */
     public Adapter getAdpater() {
         return adapter;
     }
 
-
-
-    /**
-     * ��ȡ����¼�
-     * 
-     * @return
-     */
-    public OnClickListener getOnclickListner() {
-        return onClickListener;
+    public OnItemClickListener getItemOnclickListner() {
+        return onItemClickListener;
     }
 
-	public void addFooterView(LinearLayout footerView, Object object, boolean b) {
-		// TODO Auto-generated method stub
-		addView(footerView, currentNum+1);
-		isFoot=true;
-	}
+    public void addFooterView(LinearLayout footerView) {
+        // TODO Auto-generated method stub
+        addView(footerView, currentNum+1);
+        isFoot=true;
+    }
 
-	public void removeFooterView() {
-		// TODO Auto-generated method stub
-		if(isFoot){
-		 removeViewAt(currentNum+1);
-		 isFoot=false;
-		}
-	}
+    public void removeFooterView() {
+        // TODO Auto-generated method stub
+        if(isFoot){
+            removeViewAt(currentNum+1);
+            isFoot=false;
+        }
+    }
 
-
+    public interface OnItemClickListener{
+        void onClick(View v,int position);
+    }
 
 }
