d53348789594c59a0d578a93c54e9c90d1717e85
Mason Liu <280682800@qq.com>
2015-04-29 11:07:30 +0800

    Update LinearLayoutForListView.java
    可以使用adapter.notify 来更新view

--- src/com/mentor/firstdemo/view/LinearLayoutForListView.java
+++ src/com/mentor/firstdemo/view/LinearLayoutForListView.java
@@ -1,9 +1,7 @@
-package com.mentor.firstdemo.view;
-
-
-import com.mentor.firstdemo.util.LogUtil;
+package me.ele.components;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Adapter;
@@ -13,8 +11,9 @@ public class LinearLayoutLikeListView extends LinearLayout {
 
     private Adapter adapter;
     private OnItemClickListener onItemClickListener = null;
-    private int currentNum=-1;
-    private boolean isFoot=false;
+    private int currentNum = -1;
+    private boolean isFoot = false;
+    private AdapterDataSetObserver mDataSetObserver;
 
     public LinearLayoutLikeListView(Context context) {
         super(context);
@@ -26,7 +25,7 @@ public class LinearLayoutLikeListView extends LinearLayout {
         init();
     }
 
-    public void init(){
+    public void init() {
         setOrientation(LinearLayout.VERTICAL);
     }
 
@@ -35,17 +34,23 @@ public class LinearLayoutLikeListView extends LinearLayout {
     }
 
     public void setAdapter(Adapter adpater) {
-        if (adpater==null)return;
+        if (adpater == null) return;
+        if (this.adapter != null && mDataSetObserver != null) {
+            this.adapter.unregisterDataSetObserver(mDataSetObserver);
+        }
         this.adapter = adpater;
+        this.mDataSetObserver = new AdapterDataSetObserver();
+        this.adapter.registerDataSetObserver(mDataSetObserver);
         bindLinearLayout();
     }
+
     /**
      * addview
      */
     public void bindLinearLayout() {
         int count = adapter.getCount();
-        for (int i = currentNum+1; i < count; i++) {
-            final int a=i;
+        for (int i = currentNum + 1; i < count; i++) {
+            final int a = i;
             View v = adapter.getView(i, null, null);
             v.setOnClickListener(new OnClickListener() {
                 @Override
@@ -54,29 +59,22 @@ public class LinearLayoutLikeListView extends LinearLayout {
                 }
             });
             v.setVisibility(View.VISIBLE);
-            currentNum=i;
+            currentNum = i;
             addView(v, i);
         }
     }
 
-    public void updateAdapter() {
-        try{
-            bindLinearLayout();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
+    @Deprecated
     public void updateAllAdapter() {
         try {
-            if (isFoot){
-                removeViews(0,getChildCount()-1);
-            }else{
+            if (isFoot) {
+                removeViews(0, getChildCount() - 1);
+            } else {
                 removeAllViews();
             }
-            currentNum=-1;
+            currentNum = -1;
             bindLinearLayout();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -89,22 +87,40 @@ public class LinearLayoutLikeListView extends LinearLayout {
         return onItemClickListener;
     }
 
+    /**
+     * 目前只能添加一个footer
+     *
+     * @param footerView
+     */
     public void addFooterView(LinearLayout footerView) {
         // TODO Auto-generated method stub
-        addView(footerView, currentNum+1);
-        isFoot=true;
+        if (!isFoot) {
+            addView(footerView, currentNum + 1);
+            isFoot = true;
+        }
     }
 
     public void removeFooterView() {
         // TODO Auto-generated method stub
-        if(isFoot){
-            removeViewAt(currentNum+1);
-            isFoot=false;
+        if (isFoot) {
+            removeViewAt(currentNum + 1);
+            isFoot = false;
         }
     }
 
-    public interface OnItemClickListener{
-        void onClick(View v,int position);
+    public interface OnItemClickListener {
+        void onClick(View v, int position);
     }
 
+    class AdapterDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            updateAllAdapter();
+        }
+
+        @Override
+        public void onInvalidated() {
+            updateAllAdapter();
+        }
+    }
 }
